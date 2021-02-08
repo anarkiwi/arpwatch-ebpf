7368eaf2ac2e9d1a1fe7439a8e2c7f9a58c0395e
Josh Bailey <josh@vandervecken.com>
2021-02-09 11:43:14 +1300

    update for ring buffer.

--- arpwatch.py
+++ arpwatch.py
@@ -1,40 +1,26 @@
 #!/usr/bin/python3
 
+import datetime
 import ipaddress
 import json
-import re
-import subprocess
-import sys
 import time
-import psutil
 from netaddr import EUI
 
-MAP = 'isat_map'
+logfile = '/var/log/arpwatch-ebpf-json.log'
+ip_to_mac = {}
 
-map_out = subprocess.run(['bpftool', 'map', 'list'], capture_output=True)
-map_re = re.compile(r'^(\d+):\s+hash\s+name\s+%s' % MAP)
-stdout = map_out.stdout.decode('UTF-8')
-map_match = map_re.match(stdout)
-if not map_match:
-    print('no map', stdout)
-    sys.exit(-1)
-map_id = map_match.group(1)
-
-while True:
-  print()
-  dump_out = subprocess.run(['bpftool', 'map', 'dump', 'id', str(map_id)], capture_output=True)
-  stdout = dump_out.stdout.decode('UTF-8')
-  dump = json.loads(stdout)
-  now = (time.time() - psutil.boot_time()) * 1e9
-  for entry in dump:
-      value = entry['value']
-      ip = value['target_ip']
-      if set(ip[4:]) == {0}:
-        ip = ip[:4]
-      ip = ipaddress.ip_address(int.from_bytes(bytes(ip), signed=False, byteorder='big'))
-      mac = value['target_mac']
-      mac = EUI(':'.join(['%2.2x' % i for i in mac]))
-      ktime = value['observed_ktime']
-      print('%32.32s %24.24s %10.1f sec' % (ip, mac, (now - ktime) / 1e9))
-  time.sleep(1)
-
+with open(logfile) as f:
+    while True:
+        line = f.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        json_line = json.loads(line)
+        observed_time = datetime.datetime.fromtimestamp(json_line['observed_time'])
+        target_mac = EUI(json_line['target_mac'])
+        target_ip = ipaddress.ip_address(json_line['target_ip'])
+        ip_to_mac[target_ip] = (target_mac, observed_time)
+        print()
+        for target_ip, mac_entry in sorted(ip_to_mac.items()):
+            mac, observed_time = mac_entry
+            print('\t'.join([str(i) for i in (target_ip, mac, observed_time)]))
